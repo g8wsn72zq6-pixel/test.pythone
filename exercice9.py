1b779e26161fc3308293f18820bf9ae562ba7d6c
azdineh <g8wsn72zq6-pixel>
2026-01-29 13:47:23 +0100

    Ajout du code de l'exercice 9

---
+++ exercice9.py
@@ -0,0 +1,10 @@
+#exercice9.py
+import random
+
+def generer_mot_de_passe():
+    caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    mot_de_passe = ""
+    for i in range(8):
+        mot_de_passe += random.choice(caracteres)
+    return mot_de_passe
+print("mot de passe genere :",generer_mot_de_passe())
